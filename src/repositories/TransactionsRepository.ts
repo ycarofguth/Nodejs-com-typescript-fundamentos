4a645235f3874e932cfc1c45ae79786ec40c963b
Ycaro Felipe Lima Guth <ycarofguth@gmail.com>
2020-04-15 14:00:05 -0300

    desafio resolvido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, element) => {
+      if (element.type === 'income') {
+        return (total += element.value);
+      }
+      return total;
+    }, 0);
+
+    const outcome = this.transactions.reduce((total, element) => {
+      if (element.type === 'outcome') {
+        return (total += element.value);
+      }
+      return total;
+    }, 0);
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
